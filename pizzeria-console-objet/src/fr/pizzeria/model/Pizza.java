4b64b74e5fb085726ba7639ae5f31f9f9aa68ccd
Shang PAGOTTO <shang.pagotto@gmail.com>
2016-04-29 17:31:19 +0200

    annotation

--- pizzeria-console-objet/src/fr/pizzeria/model/Pizza.java
+++ pizzeria-console-objet/src/fr/pizzeria/model/Pizza.java
@@ -1,91 +1,121 @@
 package fr.pizzeria.model;
-/*Principales caractéristiques d’une classe
 
-– nom,– liste d'attributs – liste de méthodes
+import java.lang.reflect.Field;
 
-• Déclaration avec le mot-clé class
+//@Retention(RetentionPolicy.RUNTIME)
+//@Target(ElementType.TYPE)
+//
+///*
+// * for(Field f: Pizza.class.getDeclaredField()){ System.out.println(F)
+// * DesactiveOptionMenu annotation = f.getAnnotation(DesactiverOptionMenu.class)
+// * if(annotation==null) {f.get....
+// */
+//
+//public @interface ToString {
+//	String attribus() default "";
+//}
 
-[modificateurs] class ExempleDeClasse {
-
- attributs et méthodes}*/
+/*
+ * Principales caractéristiques d’une classe– nom,– liste d'attributs – liste de
+ * méthodes • Déclaration avec le mot-clé class
+ * 
+ * [modificateurs] class ExempleDeClasse {attributs et méthodes}
+ */
 public class Pizza {
 	public int id;
+	@ToString
 	public String code;
+	@ToString
 	public String nom;
 	public double prix;
 	public CategoriePizza categorie;
-	
-	
+
 	public static int nbPizzas;
+
 	public Pizza() {
 		super();
-		//TODO Auto-generated constructeur
-		//this("jjj","hhhh",12.0);
-		
+
 	}
-	public Pizza(String code, String nom, double prix,CategoriePizza cat)
-	{
+
+	public Pizza(String code, String nom, double prix, CategoriePizza cat) {
 		this();
-		this.code=code;
-		this.nom=nom;
-		this.prix=prix;
-		this.categorie=cat;
+		this.code = code;
+		this.nom = nom;
+		this.prix = prix;
+		this.categorie = cat;
 	}
 
-	public int getId(){
-		return id;}
-	
-	
-public void setId(int id){
-	this.id=id;
-}
+	public int getId() {
+		return id;
+	}
 
-	
-	public String getCode(){
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
 		return code;
 	}
-	
-	public void setCode(String code){
-		this.code=code;
+
+	public void setCode(String code) {
+		this.code = code;
 	}
-	
-	public String getNom(){
+
+	public String getNom() {
 		return nom;
 	}
-	
-	public void setNom(String nom)
-	{
-		this.nom=nom;
-		
+
+	public void setNom(String nom) {
+		this.nom = nom;
+
 	}
-	
-	public double getprix()
-	{
-		return prix;	
-		
+
+	public double getprix() {
+		return prix;
+
 	}
-	
-	public void setPrix(double prix){
-		this.prix=prix;
+
+	public void setPrix(double prix) {
+		this.prix = prix;
 	}
-	
+
 	public static int getNbPizzas() {
 		return nbPizzas;
 	}
+
 	public static void setNbPizzas(int nbPizzas) {
 		Pizza.nbPizzas = nbPizzas;
 	}
+
 	public double getPrix() {
 		return prix;
 	}
+
 	public CategoriePizza getCategorie() {
 		return categorie;
 	}
+
 	public void setCategorie(CategoriePizza categorie) {
 		this.categorie = categorie;
 	}
+
 	@Override
 	public String toString() {
-		return "Pizza [code=" + code + ", nom=" + nom + ", prix=" + prix + ", categorie=" + categorie + "]";
+
+		String value = "";
+		for (Field champ : this.getClass.getDeclaredField())
+
+		{
+			ToString annotation = champ.getAnnotation(ToString.class);
+			/*
+			 * if(annotation!==null) { try { boolean
+			 * attribus=annotation.attribus(); Object
+			 * valeurDuChamp=champ.get(this);
+			 * 
+			 * value+= attribus?valeurDuChamp.toString().toUpperCase() }
+			 * catch(IllegalArgumentException) } } }
+			 */
+			return "Pizza [code=" + code + ", nom=" + nom + ", prix=" + prix + ", categorie=" + categorie + "]";
+		}
 	}
 }
