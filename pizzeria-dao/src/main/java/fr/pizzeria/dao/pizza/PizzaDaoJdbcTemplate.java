798f636fb6f6a114788a18b4a740453213012353
Shang PAGOTTO <shang.pagotto@gmail.com>
2016-06-02 10:16:06 +0200

    PizzaDaoJdbcTemplate

--- pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/PizzaDaoJdbcTemplate.java
+++ pizzeria-dao/src/main/java/fr/pizzeria/dao/pizza/PizzaDaoJdbcTemplate.java
@@ -1,13 +1,17 @@
 package fr.pizzeria.dao.pizza;
 
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.collections4.ListUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.TransactionTemplate;
@@ -16,60 +20,80 @@ import fr.pizzeria.exception.DaoException;
 import fr.pizzeria.model.CategoriePizza;
 import fr.pizzeria.model.Pizza;
 
+@Repository
 public class PizzaDaoJdbcTemplate implements IPizzaDao {
 
 	private static final Logger LOG = Logger.getLogger(PizzaDaoJdbcTemplate.class.toString());
 
 	private JdbcTemplate jdbcTemplate;
 
-	private TransactionTemplate transactionTemplate;
-
 	private TransactionTemplate txTemplate;
 
+	private RowMapper<Pizza> rowMapperPizza = (rs, rowNum) -> new Pizza(rs.getString("code"), rs.getString("nom"),
+			rs.getBigDecimal("prix"), CategoriePizza.valueOf(rs.getString("categorie")));
+
 	@Autowired
-	public PizzaDaoJdbcTemplate(DataSource dataSource, PlatformTransactionManager transactionManager) {
-		transactionTemplate = new TransactionTemplate(transactionManager);
-		jdbcTemplate = new JdbcTemplate(dataSource);
+	public PizzaDaoJdbcTemplate(DataSource dataSource, PlatformTransactionManager txManager) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+		this.txTemplate = new TransactionTemplate(txManager);
+		LOG.log(Level.INFO, "Création du bean PizzaDaoJdbcTemplate");
 	}
 
 	@Override
 	public List<Pizza> findAllPizzas() throws DaoException {
-		String sql = "SELECT*FROM pizza";
-		return jdbcTemplate.query(sql, (rs, rowNum) -> new Pizza(rs.getString("reference"), rs.getString("libelle"),
-				rs.getDouble("prix"), CategoriePizza.valueOf(rs.getString("categorie"))));
-
+		return this.jdbcTemplate.query("select * from pizza", rowMapperPizza);
 	}
 
 	@Override
-	public void savePizza(Pizza newPizza) throws DaoException {
-		this.jdbcTemplate.update("insert into pizza(code, nom, prix, catégorie, url_image)VALUES(?,?,?,?,?)",
+	public Pizza findOnePizza(String code) throws DaoException {
+		try {
+			return this.jdbcTemplate.queryForObject("select * from pizza where code=?", rowMapperPizza, code);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
+
+	@Override
+	public void savePizza(Pizza newPizza) {
+		this.jdbcTemplate.update("insert into pizza (code,nom,prix,categorie,url_image) VALUES(?,?,?,?,?)",
 				newPizza.getCode(), newPizza.getNom(), newPizza.getPrix(), newPizza.getCategorie().name(),
 				newPizza.getUrlImage());
 	}
 
 	@Override
 	public void updatePizza(String codePizza, Pizza updatePizza) throws DaoException {
-		this.jdbcTemplate.update("update pizza set code=?, nom=?,prix=?,categorie=?,url_image=?", updatePizza.getCode(),
-				updatePizza.getNom(), updatePizza.getPrix(), updatePizza.getCategorie().name(),
-				updatePizza.getUrlImage());
+		this.jdbcTemplate.update("update pizza set code = ?, nom = ?,prix=?,categorie=?,url_image=? where code=?",
+				updatePizza.getCode(), updatePizza.getNom(), updatePizza.getPrix(), updatePizza.getCategorie().name(),
+				updatePizza.getUrlImage(), codePizza);
 	}
 
 	@Override
 	public void deletePizza(String codePizza) throws DaoException {
-		this.jdbcTemplate.update("delete from pizza where code =?", codePizza);
+		this.jdbcTemplate.update("delete from pizza where code=?", codePizza);
 	}
 
 	@Override
 	public void saveAllPizzas(List<Pizza> listPizzas, int nb) throws DaoException {
+
 		ListUtils.partition(listPizzas, nb).forEach(list -> {
 
-			this.transactionTemplate.execute(status -> {
-				list.foreach(p -> this.savePizza(p));
+			/*
+			 * this.txTemplate.execute(new TransactionCallback<Pizza>() {
+			 * 
+			 * @Override public Pizza doInTransaction(TransactionStatus status)
+			 * { return null; } });
+			 */
+
+			this.txTemplate.execute(status -> {
+				list.forEach(p -> this.savePizza(p));
+				return null;
 			});
+
 		});
 
 	}
 
+	@Override
 	public Pizza doInTransaction(TransactionStatus status) {
 		// TODO Auto-generated method stub
 		return null;
--- pizzeria-dao/src/test/java/fr/pizzeria/dao/PizzaDaoJdbcTemplateTest.java
+++ pizzeria-dao/src/test/java/fr/pizzeria/dao/PizzaDaoJdbcTemplateTest.java
@@ -1,27 +1,14 @@
 package fr.pizzeria.dao;
 
-import java.util.List;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.beans.factory.annotation.Qualifier;
 
-import fr.pizzeria.dao.pizza.PizzaDaoJdbcTemplate;
-import fr.pizzeria.exception.DaoException;
-import fr.pizzeria.model.Pizza;
+import fr.pizzeria.dao.pizza.IPizzaDao;
 
+public class PizzaDaoJdbcTemplateTest extends PizzaDaoTest {
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=PizzaDaoSpringTest.class)
-public class PizzaDaoJdbcTemplateTest {
-	@Autowired private PizzaDaoJdbcTemplate pizzaDaoJdbcTemplate;
-	@Test
-	public void testfindAllPizzas()throws DaoException{
-		List<Pizza> pizza = pizzaDaoJdbcTemplate.findAllPizzas();
-			Assert.assertEquals(6, pizza.size());
+	@Autowired
+	public void setPIzzaDao(@Qualifier("pizzaDaoJdbcTemplate") IPizzaDao pizzaDao) {
+		this.pizzaDao = pizzaDao;
 	}
-
 }
